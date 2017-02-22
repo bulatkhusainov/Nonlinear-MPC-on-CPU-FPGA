04d10366dd82cebcf3c4d4163f9a885b5b140eaa
Bulat Khusainov <bulat_khusainov@mail.ru>
2017-02-22 17:17:45 +0000

    started implementing MINRES in HW

--- src/user_minres_HW.c
+++ src/user_minres_HW.c
@@ -3,7 +3,6 @@
 #include <math.h>  
 #include "mex.h"
 
-
 float vv_mult(float *x_1, float *x_2)
 {
 	int i;
@@ -15,11 +14,8 @@ float vv_mult(float *x_1, float *x_2)
 	return sos;
 }
 
-#ifdef MINRES_prescaled
-	void minres(float* blocks, float* out_blocks, float* b,float* x_current)
-#else
-	void minres(float* blocks, float* b,float* x_current)
-#endif
+
+void minres(float* blocks, float* out_blocks, float* b,float* x_current)
 {
 
 
@@ -27,7 +23,7 @@ float vv_mult(float *x_1, float *x_2)
 	int counter = 1, i; 
 	float x_new[n_linear];
 		//x_current[n_linear] // x_current is a solution guess (is coming from function interface)
-	float *v_current, v_tmp1[n_linear], v_tmp2[n_linear]; // Lanczos vectors for SW implementation
+	float v_current_HW_in[n_linear];
 	float v_current_HW[n_linear]; // v_current for HW realization
 	float v_current_alg[n_linear]; // v_current for MINRES algorithm
 	float beta_current, over_beta_current, beta_new, beta_nnew;
@@ -39,41 +35,28 @@ float vv_mult(float *x_1, float *x_2)
 	float delta, over_ro_1, ro_2, ro_3;
 	float sc_in[5], sc_out[5];
 
-	// declare and define pointers
-	float *x_current_p = x_current; // x arrays
-	float *x_new_p = x_new;
-	float *omega_pprev_p = omega_pprev; // omega arrays
-	float *omega_prev_p = omega_prev;
-	float *omega_current_p = omega_current;
-	float *tmp_pointer;
 
 	//initialisation
 	for(i = 0; i < n_linear; i++)
-		omega_pprev_p[i] = 0;
+		omega_pprev[i] = 0;
 	for(i = 0; i < n_linear; i++)
-		omega_prev_p[i] = 0;
+		omega_prev[i] = 0;
 	for(i = 0; i < n_linear; i++)
-		x_current_p[i] = 0;
+		x_current[i] = 0;
 	for(i = 0; i < n_linear; i++)
-		v_tmp1[i] = 0; // initially this vector stores v_prev
-	for(i = 0; i < n_linear; i++)
-		v_tmp2[i] = b[i]; // initially this vector stores v_current
+		v_current_HW_in[i] = b[i]; // initially this vector stores v_current
 
-	beta_current = sqrtf(vv_mult(v_tmp2,v_tmp2));
+	beta_current = sqrtf(vv_mult(v_current_HW_in,v_current_HW_in));
 	nu_current = beta_current;
 
 	// manually perform first iteration of Lanczos kernel to achieve pipelining
 	for(i = 0; i < n_linear; i++)
-		v_tmp2[i] = v_tmp2[i]/beta_current;
+		v_current_HW_in[i] = v_current_HW_in[i]/beta_current;
 	sc_in[0] = beta_current;
 	#ifdef MINRES_prescaled
-		#if heterogeneity > 1
-			wrap_lanczos_HW(1, blocks, out_blocks, v_tmp2, v_current_HW, sc_in, sc_out); // HW implementation
-		#else
-			lanczos(1, blocks, out_blocks, v_tmp1, v_tmp2, &v_current, sc_in, sc_out); // SW implementation
-		#endif
+		lanczos_HW(1, &blocks, out_blocks, v_current_HW_in,  v_current_HW, sc_in, sc_out);
 	#else
-		lanczos(1, blocks, v_tmp1, v_tmp2, &v_current, sc_in, sc_out);
+		// no hardware realization of unprescaled implementation
 	#endif
 
 	// main loop
@@ -82,27 +65,17 @@ float vv_mult(float *x_1, float *x_2)
 
 
 		// extract reqired info from the lanczos kernel
-		#if heterogeneity > 1
-			for(i = 0; i < n_linear; i++)
+		for(i = 0; i < n_linear; i++)
 				v_current_alg[i] = v_current_HW[i];
-		#else
-			for(i = 0; i < n_linear; i++)
-				v_current_alg[i] = v_current[i];
-		#endif
 		alfa_current = sc_out[0];
 		beta_current = sc_out[1];
 		beta_new = sc_out[2];
 
-
 		// start a new iteration of Lanczos kernel (this function operates in parallel with the rest of MINRES ieration)
 		#ifdef MINRES_prescaled
-			#if heterogeneity > 1
-				wrap_lanczos_HW(0, blocks, out_blocks, v_tmp2, v_current_HW, sc_in, sc_out); // HW implementation
-			#else
-				lanczos(0, blocks, out_blocks, v_tmp1, v_tmp2, &v_current, sc_in, sc_out); // SW implementation
-			#endif
+			lanczos_HW(0, &blocks, out_blocks, v_current_HW_in,  v_current_HW, sc_in, sc_out);
 		#else
-			lanczos(0, blocks, v_tmp1, v_tmp2, &v_current, sc_in, sc_out);
+			// no hardware realization of unprescaled implementation
 		#endif
 
 		// Calculate QR factors
@@ -117,20 +90,23 @@ float vv_mult(float *x_1, float *x_2)
 
     	//update solution
     	for(i = 0;i < n_linear; i++)
-    		omega_current_p[i] = (v_current_alg[i] - ro_3*omega_pprev_p[i] - ro_2*omega_prev_p[i])*over_ro_1;
+    		omega_current[i] = (v_current_alg[i] - ro_3*omega_pprev[i] - ro_2*omega_prev[i])*over_ro_1;
     	for(i = 0; i < n_linear; i++)
-    		x_new_p[i] = x_current_p[i] + gamma_new*nu_current*omega_current_p[i];
+    		x_new[i] = x_current[i] + gamma_new*nu_current*omega_current[i];
     	nu_new = -sigma_new*nu_current;
 
-    	// update variables and pointers
-		tmp_pointer = x_current_p; // update pointers the for x
-		x_current_p = x_new_p;
-		x_new_p = tmp_pointer;
 
-		tmp_pointer = omega_pprev_p; // udpdate pointers for omega
-		omega_pprev_p = omega_prev_p;
-		omega_prev_p = omega_current_p;
-		omega_current_p = tmp_pointer;
+    	// update variables and pointers
+		for(i = 0; i < n_linear; i++)
+		{
+			x_current[i] = x_new[i];
+		}
+
+		for(i = 0; i < n_linear; i++)
+		{
+			omega_pprev[i] = omega_prev[i];
+			omega_prev[i] = omega_current[i];
+		}
  	
     	nu_current = nu_new; // update variables
 
